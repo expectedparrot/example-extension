a9c9943cdec323cdc097a366f6a9b6cb8c4699d7
John Horton <john.joseph.horton@gmail.com>
2025-04-27 20:18:39 -0400

    Remove excess line

--- example_variant.py
+++ example_variant.py
@@ -4,8 +4,6 @@ from edsl.extensions import extensions, register_service
 
 router = APIRouter()
 
-router = APIRouter()
-
 @register_service(router, "create_survey", extensions["create_survey"])
 async def create_survey_logic(overall_question, population, ep_api_token):
     """Return a dict that matches service_def’s response schema."""
